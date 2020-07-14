6aabe49692b935ae764566a19329a0189d56dae1
“Milfist” <milfist@gmail.com>
2020-07-14 08:19:59 +0200

    alias updated

--- terminal/_alias/alias.sh
+++ terminal/_alias/alias.sh
@@ -1,4 +1,13 @@
-alias ll='ls -lGa'
-alias ls='ls -GFh'
+# directory
+alias ll='exa -la'
+alias ls='exa -l'
 alias ..='cd ..'
+alias ...='cd ../..'
+alias tt="exa --tree --level=2 --long"
+
+alias dot='cd ~/.dotfiles'
+alias g='cd ~/Git'
+alias mik='cd ~/Git/mik'
+
+# others
 alias k=kubectl
